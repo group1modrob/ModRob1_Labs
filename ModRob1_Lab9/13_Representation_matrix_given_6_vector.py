e3cb1ccd4211cc9f142aeeacd9884f11dcaa6e43
longtaichen <143827458+longtaichen@users.noreply.github.com>
2023-10-14 11:01:23 -0500

    Update 13_Representation_matrix_given_6_vector.py

--- ModRob1_Lab9/13_Representation_matrix_given_6_vector.py
+++ ModRob1_Lab9/13_Representation_matrix_given_6_vector.py
@@ -3,6 +3,7 @@ import numpy as np
 # input the screw axis here
 S = np.array([[0],[0.447],[0.8944],[1.345],[0],[0]])
 
+# compute the representation matrix
 representation_matrix = np.array([[0,-S[2,0],S[1,0],S[3,0]],[S[2,0],0,-S[0,0],S[4,0]],[-S[1,0],S[0,0],0,S[5,0]],[0,0,0,0]])
 
 # print te result
